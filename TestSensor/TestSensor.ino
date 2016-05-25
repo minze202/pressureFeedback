469e752f5773bf5d7995d1be7dccc24a010c1238
minze202 <santeko12@web.de>
2016-05-25 15:10:39 +0200

    Without inputMode

--- TestSensor/TestSensor.ino
+++ TestSensor/TestSensor.ino
@@ -1,21 +1,17 @@
 
       
 void setup() {
-    Serial.begin(9600);  // initialize serial communication at 9600 bits per second:
+  // initialize serial communications at 9600 bps:
+  Serial.begin(9600);
 }
 
-void loop() {   // the loop routine runs over and over again forever:
+void loop() {
+  
+  int sensorValue = analogRead(A5);
 
-   long int sensorValue = analogRead(A5); // read the input on analog pin 0:
-   long int tensione, P, Pmbar;
-    // print out the value you read:
-   //'tensione= sensorValue*0.0049;'
-   //'P=(sensorValue*0.0049/3.3+0.095)/0.009;'
-   //Pmbar=((analogRead(A5)*(0.0048828125)/4.8)+0.095)/0.0009;
-   
-   //Serial.println(tensione);
-   //Serial.println(P);
-   //Serial.println(Pmbar);
-   Serial.println(sensorValue);
-   delay(500); // delay in between reads for stability
+  // print the results to the serial monitor:
+  Serial.print("sensor = ");
+  Serial.println(sensorValue);
+
+  delay(1000);
 }
